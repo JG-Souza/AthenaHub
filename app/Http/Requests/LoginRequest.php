f2b160814e16ab0ee4bf327254b6382525a92cbf
JG-Souza <joaogsouzafma@gmail.com>
2025-06-07 22:31:58 -0300

    Implementa `AuthController`, `CheckRole` middleware e divide as atribuições dos Requests

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AuthRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,12 +22,8 @@ class AuthRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string'],
-            'email' => ['required', 'string', 'email', 'unique:users,email'],
+            'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string', 'min:8'],
-            'phone' => ['required', 'string'],
-            'role' => ['required', 'in:student,teacher,admin'],
-            'cpf' => ['required', 'string', 'size:11', 'unique:users,cpf'],
         ];
     }
 }
--- database/migrations/2025_06_04_171934_update_users_table.php
+++ database/migrations/2025_06_04_171934_update_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('phone');
-            $table->enum('role', ['student', 'teacher', 'admin']);
+            $table->enum('role', ['student', 'teacher', 'admin'])->default('student');
             $table->string('cpf');
         });
     }
